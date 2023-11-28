95b64ebad02dd75bc55a0aed0b28531cb5c3aa53
rayan <37596@ma-web.nl>
2023-11-28 12:32:39 +0100

    huis

---
+++ 02 huisje/app.js
@@ -0,0 +1,31 @@
+class App 
+{
+    runApplication()
+    {
+
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+
+            g.fillStyle = "red";
+            g.fillRect(50, 50, 300, 300);
+
+
+            g.fillStyle = "black";
+            g.beginPath();
+            g.moveTo(50, 50);
+            g.lineTo(200, 10);
+            g.lineTo(350, 50);
+            g.closePath();
+            g.fill();
+
+            g.fillStyle = "white";
+            g.fillRect(150, 200, 100, 150);
+
+
+            g.fillStyle = "white"; //
+            g.fillRect(80, 80, 60, 60);
+
+    }
+}
+let app = new App();
+app.runApplication();
