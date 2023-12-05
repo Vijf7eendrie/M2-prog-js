46c206a4e01e7b7d9c84726791900f22fa5385fb
rayan <37596@ma-web.nl>
2023-12-05 20:17:28 +0100

    opdracht

--- 04functions/app.js
+++ 04functions/app.js
@@ -1,19 +1,27 @@
-
-    function superMooieGlobalFunction()
+    function superMooieGlobalFunction()    
     {
         console.log("ik ben global");
         console.log("dus je mag mij overal aanroepen");
 
     }
-    class app
+    superMooieGlobalFunction();
+    superMooieGlobalFunction();
+    superMooieGlobalFunction();
+function aanroepVoorbeeld()
+{
+}
+   class App
     {
         runApplication()
         { 
-            console.log("hello world!")
+            console.log("hello world!");
+            superMooieGlobalFunction();
+            
         }
     }
 
-    let app = new app();
-    app.runApplication();
+
+let app = new App();
+app.runApplication();
 
     
