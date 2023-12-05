50d1dd5c96e8c67df8097c6f4b8ac3a19fed34c1
rayan <37596@ma-web.nl>
2023-12-05 20:06:19 +0100

    functies

---
+++ 04functions/app.js
@@ -0,0 +1,19 @@
+
+    function superMooieGlobalFunction()
+    {
+        console.log("ik ben global");
+        console.log("dus je mag mij overal aanroepen");
+
+    }
+    class app
+    {
+        runApplication()
+        { 
+            console.log("hello world!")
+        }
+    }
+
+    let app = new app();
+    app.runApplication();
+
+    
