a85c3fda124d3e4088773de3212bf603ab6fd1bb
rayan <37596@ma-web.nl>
2023-12-05 20:41:52 +0100

    commit

--- 04functions/app.js
+++ 04functions/app.js
@@ -1,12 +1,49 @@
-    function superMooieGlobalFunction()    
+ function argumentsAreHandy(shoutout) 
+   
+{   
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+}
+argumentsAreHandy("crazy shoutout");
+
+function argumentsAreHandy1(shoutout1) 
+{   
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout1);
+}
+argumentsAreHandy1("insane shoutout");
+
+
+
+function argumentsAreHandy2(shoutout2) 
+{   
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout2);
+}
+argumentsAreHandy1("gekke shoutout");
+
+
+
+
+
+
+
+
+superMooieGlobalFunction();
+
+
+   function superMooieGlobalFunction()    
     {
         console.log("ik ben global");
         console.log("dus je mag mij overal aanroepen");
 
     }
-    superMooieGlobalFunction();
-    superMooieGlobalFunction();
-    superMooieGlobalFunction();
+        superMooieGlobalFunction();
+        superMooieGlobalFunction();
+        superMooieGlobalFunction();
+
+
+
 function aanroepVoorbeeld()
 {
 }
